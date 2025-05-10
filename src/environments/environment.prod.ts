f192604e021c0ac09452525d1c134582c7bf08d5
Châu Chí Huy <120136224+chauchihuy@users.noreply.github.com>
2025-05-10 21:48:41 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,            
-    apiBaseUrl: 'http://localhost:8099/api/v1',
+    production: true,
+    // apiBaseUrl: 'http://localhost:8088/api/v1', 
+     apiBaseUrl: 'https://backend-book-9aeq.onrender.com/api/v1',        
 };
